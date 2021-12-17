b217469d5b8a6045bbb9006784e93ecee706f950
wilson Susanto <wilsonzhaang666@gmail.com>
2021-12-17 17:16:25 +1100

    added better empty cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -6,40 +6,40 @@ import { useHistory } from "react-router-dom";
 import { Add, Remove } from "@material-ui/icons";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import { IconButton } from '@material-ui/core';
+import { IconButton } from "@material-ui/core";
 import { mobile } from "../responsive";
 const Cart = () => {
   const Container = styled.div``;
 
-const Wrapper = styled.div`
-  padding: 20px;
-  ${mobile({ padding: "10px" })}
-`;
+  const Wrapper = styled.div`
+    padding: 20px;
+    ${mobile({ padding: "10px" })}
+  `;
 
-const Title = styled.h1`
-  font-weight: 300;
-  text-align: center;
-`;
+  const Title = styled.h1`
+    font-weight: 300;
+    text-align: center;
+  `;
 
-const Top = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 20px;
-`;
+  const Top = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px;
+  `;
 
-const TopButton = styled.button`
-      text-decoration: none;
-      color: inherit;
-  padding: 10px;
-  border: 1px solid ;
+  const TopButton = styled.button`
+    text-decoration: none;
+    color: inherit;
+    padding: 10px;
+    border: 1px solid;
 
-  font-weight: 600;
-  cursor: pointer;
-  background-color:transparent;
-`;
+    font-weight: 600;
+    cursor: pointer;
+    background-color: transparent;
+  `;
 
-const TopButton1 = styled.button`
+  const TopButton1 = styled.button`
       text-decoration: none;
       color: inherit;
   padding: 10px;
@@ -49,208 +49,237 @@ const TopButton1 = styled.button`
   background-color:black};
   color:white;
 `;
-const TopTexts = styled.div`
-  ${mobile({ display: "none" })}
-`;
-const TopText = styled.span`
-  text-decoration: underline;
-  cursor: pointer;
-  margin: 0px 10px;
-`;
+  const TopTexts = styled.div`
+    ${mobile({ display: "none" })}
+  `;
+  const TopText = styled.span`
+    text-decoration: underline;
+    cursor: pointer;
+    margin: 0px 10px;
+  `;
 
-const Bottom = styled.div`
-  display: flex;
-  justify-content: space-between;
-  ${mobile({ flexDirection: "column" })}
+  const Bottom = styled.div`
+    display: flex;
+    justify-content: space-between;
+    ${mobile({ flexDirection: "column" })}
+  `;
 
-`;
+  const Info = styled.div`
+    flex: 3;
+  `;
 
-const Info = styled.div`
-  flex: 3;
-`;
+  const Product = styled.div`
+    display: flex;
+    justify-content: space-between;
+    ${mobile({ flexDirection: "column" })}
+  `;
 
-const Product = styled.div`
-  display: flex;
-  justify-content: space-between;
-  ${mobile({ flexDirection: "column" })}
-`;
+  const ProductDetail = styled.div`
+    flex: 2;
+    display: flex;
+  `;
 
-const ProductDetail = styled.div`
-  flex: 2;
-  display: flex;
-`;
+  const Image = styled.img`
+    width: 200px;
+  `;
 
-const Image = styled.img`
-  width: 200px;
-`;
+  const Details = styled.div`
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+  `;
 
-const Details = styled.div`
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-`;
+  const ProductName = styled.span``;
 
-const ProductName = styled.span``;
+  const ProductId = styled.span``;
 
+  const ProductColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${(props) => props.color};
+  `;
 
-const ProductId = styled.span``;
+  const ProductSize = styled.span``;
 
-const ProductColor = styled.div`
-  width: 20px;
-  height: 20px;
-  border-radius: 50%;
-  background-color: ${(props) => props.color};
-`;
+  const PriceDetail = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  `;
 
-const ProductSize = styled.span``;
+  const ProductAmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+  `;
 
-const PriceDetail = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
+  const ProductAmount = styled.div`
+    font-size: 24px;
+    margin: 5px;
+    ${mobile({ margin: "5px 15px" })}
+  `;
 
-const ProductAmountContainer = styled.div`
-  display: flex;
-  align-items: center;
-  margin-bottom: 20px;
-`;
+  const ProductPrice = styled.div`
+    font-size: 30px;
+    font-weight: 200;
+    ${mobile({ marginBottom: "20px" })}
+  `;
 
-const ProductAmount = styled.div`
-  font-size: 24px;
-  margin: 5px;
-  ${mobile({ margin: "5px 15px" })}
-`;
+  const Hr = styled.hr`
+    background-color: #eee;
+    border: none;
+    height: 1px;
+  `;
 
-const ProductPrice = styled.div`
-  font-size: 30px;
-  font-weight: 200;
-  ${mobile({ marginBottom: "20px" })}
-`;
+  const Summary = styled.div`
+    flex: 1;
+    border: 0.5px solid lightgray;
+    border-radius: 10px;
+    padding: 20px;
+    height: 50vh;
+  `;
 
-const Hr = styled.hr`
-  background-color: #eee;
-  border: none;
-  height: 1px;
-`;
+  const SummaryTitle = styled.h1`
+    font-weight: 200;
+  `;
 
-const Summary = styled.div`
-  flex: 1;
-  border: 0.5px solid lightgray;
-  border-radius: 10px;
-  padding: 20px;
-  height: 50vh;
-`;
+  const SummaryItem = styled.div`
+    margin: 30px 0px;
+    display: flex;
+    justify-content: space-between;
+    font-weight: ${(props) => props.type === "total" && "500"};
+    font-size: ${(props) => props.type === "total" && "24px"};
+  `;
 
-const SummaryTitle = styled.h1`
-  font-weight: 200;
-`;
+  const SummaryItemText = styled.span``;
 
-const SummaryItem = styled.div`
-  margin: 30px 0px;
-  display: flex;
-  justify-content: space-between;
-  font-weight: ${(props) => props.type === "total" && "500"};
-  font-size: ${(props) => props.type === "total" && "24px"};
-`;
+  const SummaryItemPrice = styled.span``;
 
-const SummaryItemText = styled.span``;
+  const Button = styled.button`
+    width: 100%;
+    padding: 10px;
+    background-color: black;
+    color: white;
+    font-weight: 600;
+  `;
 
-const SummaryItemPrice = styled.span``;
-
-const Button = styled.button`
-  width: 100%;
-  padding: 10px;
-  background-color: black;
-  color: white;
-  font-weight: 600;
-`;
+  const Notification = styled.div`
+    height: 10em;
+    position: relative;
+  `;
+  const Notice = styled.h1`
+    margin: 0;
+    position: absolute;
+    top: 250%;
+    left: 50%;
+    margin-right: -50%;
+    transform: translate(-50%, -50%);
+  `;
+  const Engagement = styled.p`
+    font-weight: light;
+    font-size: 14px;
+    color: grey;
+    margin: 0;
+    position: absolute;
+    top: 280%;
+    left: 50%;
+    margin-right: -50%;
+    transform: translate(-50%, -50%);
+  `;
   const history = useHistory();
-  const { cart, total, increaseAmount, decreaseAmount } = useContext(CartContext);
+  const { cart, total, increaseAmount, decreaseAmount } =
+    useContext(CartContext);
   if (!cart.length) {
-    return <h3>Empty Cart</h3>
+    return (
+      <Notification>
+        <Notice>Empty Cart</Notice>
+        <Engagement>Go Grab some stuff and Come Back! </Engagement>
+      </Notification>
+    );
   }
- 
+
   return (
     <>
-    <Container>
+      <Container>
+        <Wrapper>
+          <Title>YOUR BAG</Title>
+          <Top>
+            <TopButton onClick={() => history.push("/cases")}>
+              CONTINUE SHOPPING
+            </TopButton>
 
-      <Wrapper>
-        
-        <Title>YOUR BAG</Title>
-        <Top>
-          <TopButton onClick={() => history.push("/cases")}>CONTINUE SHOPPING</TopButton>
+            <TopTexts>
+              <TopText>Shopping Bag</TopText>
+            </TopTexts>
+            <TopButton1 type="filled" onClick={() => history.push("/checkout")}>
+              CHECKOUT NOW
+            </TopButton1>
+          </Top>
+          <Bottom>
+            <Info>
+              {cart.map(({ id, title, price, image, amount, category }) => (
+                <>
+                  <Product>
+                    <ProductDetail key={id}>
+                      <Image src={image} />
+                      <Details>
+                        <ProductName>
+                          <b style={{ fontSize: "22px" }}>{title}</b>
+                        </ProductName>
+                        <ProductId></ProductId>
 
-          <TopTexts>
-            <TopText>Shopping Bag</TopText>
-          </TopTexts>
-          <TopButton1 type="filled"onClick={() => history.push("/checkout")}>CHECKOUT NOW</TopButton1>
-        </Top>
-        <Bottom>
-          <Info>
-          {cart.map(({ id, title, price, image, amount,category }) => (
-             <>
-
-            <Product>
-              <ProductDetail key={id}>
-                <Image src={image} />
-                <Details>
-                  <ProductName>
-                    <b style={{fontSize:"22px"}}>{title}</b> 
-                  </ProductName>
-                  <ProductId>
-
-
-                  </ProductId>
-                  
-                  <ProductSize>
-                  <b>Product ID:</b> {id}<br/>
-                    <b>Category:</b> {category}
-                  </ProductSize>
-                </Details>
-              </ProductDetail>
-              <PriceDetail>
-                <ProductAmountContainer>
-                  <Add  onClick={() => increaseAmount(id)}/>
-                  <ProductAmount>{amount}</ProductAmount>
-                  <Remove onClick={()=> decreaseAmount(id, amount)} />
-                </ProductAmountContainer>
-                <ProductPrice>$ {(price*amount).toFixed(2)}</ProductPrice>
-              </PriceDetail>
-            </Product>
-            <br/>
-            </>
-                                         ))}
-            <Hr />
-
-          </Info>
-          <Summary>
-            <SummaryTitle>ORDER SUMMARY</SummaryTitle>
-            <SummaryItem>
-              <SummaryItemText>Subtotal</SummaryItemText>
-              <SummaryItemPrice>${(total/0.8).toFixed(2)}</SummaryItemPrice>
-            </SummaryItem>
-            <SummaryItem>
-              <SummaryItemText>Tax</SummaryItemText>
-              <SummaryItemPrice>$0</SummaryItemPrice>
-            </SummaryItem>
-            <SummaryItem>
-              <SummaryItemText>Discount</SummaryItemText>
-              <SummaryItemPrice>10%</SummaryItemPrice>
-            </SummaryItem>
-            <SummaryItem type="total">
-              <SummaryItemText>Total</SummaryItemText>
-              <SummaryItemPrice>${(total).toFixed(2)}</SummaryItemPrice>
-            </SummaryItem>
-            <Button onClick={() => history.push("/checkout")}>CHECKOUT NOW</Button>
-          </Summary>
-        </Bottom>
-      </Wrapper>
-
-    </Container>
+                        <ProductSize>
+                          <b>Product ID:</b> {id}
+                          <br />
+                          <b>Category:</b> {category}
+                        </ProductSize>
+                      </Details>
+                    </ProductDetail>
+                    <PriceDetail>
+                      <ProductAmountContainer>
+                        <Add onClick={() => increaseAmount(id)} />
+                        <ProductAmount>{amount}</ProductAmount>
+                        <Remove onClick={() => decreaseAmount(id, amount)} />
+                      </ProductAmountContainer>
+                      <ProductPrice>
+                        $ {(price * amount).toFixed(2)}
+                      </ProductPrice>
+                    </PriceDetail>
+                  </Product>
+                  <br />
+                </>
+              ))}
+              <Hr />
+            </Info>
+            <Summary>
+              <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+              <SummaryItem>
+                <SummaryItemText>Subtotal</SummaryItemText>
+                <SummaryItemPrice>${(total / 0.8).toFixed(2)}</SummaryItemPrice>
+              </SummaryItem>
+              <SummaryItem>
+                <SummaryItemText>Tax</SummaryItemText>
+                <SummaryItemPrice>$0</SummaryItemPrice>
+              </SummaryItem>
+              <SummaryItem>
+                <SummaryItemText>Discount</SummaryItemText>
+                <SummaryItemPrice>10%</SummaryItemPrice>
+              </SummaryItem>
+              <SummaryItem type="total">
+                <SummaryItemText>Total</SummaryItemText>
+                <SummaryItemPrice>${total.toFixed(2)}</SummaryItemPrice>
+              </SummaryItem>
+              <Button onClick={() => history.push("/checkout")}>
+                CHECKOUT NOW
+              </Button>
+            </Summary>
+          </Bottom>
+        </Wrapper>
+      </Container>
     </>
   );
 };
