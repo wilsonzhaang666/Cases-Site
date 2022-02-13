39f0de0fc921bd24f52b7584cc20e27eb5c5ea3c
wilson Susanto <wilsonzhaang666@gmail.com>
2022-02-13 12:39:14 +1100

    added video

--- src/pages/DeliveryInfo.js
+++ src/pages/DeliveryInfo.js
@@ -1,21 +1,27 @@
-import React from 'react'
+import React from "react";
 const DeliveryInfo = () => {
+  return (
+    <>
+      <div style={{ margin: "50px", marginTop: "20px" }}>
+        <p>CASES.SITE</p>
+        <p style={{ fontSize: "34px" }}>Delivery Info</p>
+        <br />
+        <br />
+        <p>
+          We only provide delivery service at Weekend at the moment. But the
+          pick up service is available all week. Sorry for the inconvenience
+        </p>
+        <p>
+          Our Pick Up point is located in 160 Victoria St, Carlton, Victoria,
+          3053
+        </p>
+        <p>
+          We can do post over australia, but the develiver fee will depending on
+          the purchase items. (Purchase above 100 dollar will be free deliver)
+        </p>
+      </div>
+    </>
+  );
+};
 
-
-  
-    return (
-     <>
-     <div style={{margin:"50px",marginTop:"20px"}}>
-     <p>CASES.SITE</p>
-     <p style={{fontSize:"34px"}}>Delivery Info</p>
-     <br/>
-     <br/>
-     <p >We only provide delivery service at Weekend at the moment. But the pick up service is available all week. Sorry for the inconvenience</p>
-     <p >Our Pick Up point is located in 160 Victoria St, Carlton, Victoria, 3053</p>
-
-        </div>
-        </>
-    );
-  };
-  
-  export default DeliveryInfo;
+export default DeliveryInfo;
